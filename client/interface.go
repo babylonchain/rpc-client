4daafce85f57fe097fac23e7e101d4e7e1a4f5f8
Runchao Han <me@runchao.rocks>
2023-02-23 17:38:50 +1100

    chore: inherit interfaces of query client (#30)

--- client/interface.go
+++ client/interface.go
@@ -4,15 +4,18 @@ import (
 	btcctypes "github.com/babylonchain/babylon/x/btccheckpoint/types"
 	btclctypes "github.com/babylonchain/babylon/x/btclightclient/types"
 	"github.com/babylonchain/rpc-client/config"
+	bbnquery "github.com/babylonchain/rpc-client/query"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 type BabylonClient interface {
-	Stop()
+	bbnquery.BabylonQueryClient
+
 	GetConfig() *config.BabylonConfig
 	GetTagIdx() uint8
 	GetAddr() (sdk.AccAddress, error)
 	MustGetAddr() sdk.AccAddress
+
 	InsertBTCSpvProof(msg *btcctypes.MsgInsertBTCSpvProof) (*sdk.TxResponse, error)
 	InsertHeader(msg *btclctypes.MsgInsertHeader) (*sdk.TxResponse, error)
 	InsertHeaders(msgs []*btclctypes.MsgInsertHeader) (*sdk.TxResponse, error)
