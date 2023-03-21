8b2dba0aa4b5255ea2be695a05517392ecfa5f7f
Gurjot <gurjotsyd@gmail.com>
2023-03-21 18:44:41 +0530

    list validators in staking

--- query/staking.go
+++ query/staking.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/cosmos/cosmos-sdk/client"
+	sdkquerytypes "github.com/cosmos/cosmos-sdk/types/query"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
 
@@ -19,13 +20,29 @@ func (c *QueryClient) QueryStaking(f func(ctx context.Context, queryClient staki
 	return f(ctx, queryClient)
 }
 
-// QueryStakingParams queries btccheckpoint module's parameters via ChainClient
+// StakingParams queries btccheckpoint module's parameters via ChainClient
 func (c *QueryClient) StakingParams() (*stakingtypes.QueryParamsResponse, error) {
 	var resp *stakingtypes.QueryParamsResponse
 	err := c.QueryStaking(func(ctx context.Context, queryClient stakingtypes.QueryClient) error {
 		var err error
 		req := &stakingtypes.QueryParamsRequest{}
 		resp, err = queryClient.Params(ctx, req)
+		return err
+	})
+
+	return resp, err
+}
+
+// ListValidators queries the list of validators via ChainClient
+func (c *QueryClient) ListValidators(status string, pagination *sdkquerytypes.PageRequest) (*stakingtypes.QueryValidatorsResponse, error) {
+	var resp *stakingtypes.QueryValidatorsResponse
+	err := c.QueryStaking(func(ctx context.Context, queryClient stakingtypes.QueryClient) error {
+		var err error
+		req := &stakingtypes.QueryValidatorsRequest{
+			Status:     status,
+			Pagination: pagination,
+		}
+		resp, err = queryClient.Validators(ctx, req)
 		return err
 	})
 
